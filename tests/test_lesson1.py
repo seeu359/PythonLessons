3a69f7353b1cbd67769a3aeafbb756d37dc2f521
a.cheremushkin <a.ch3re359@yandex.ru>
2023-02-09 17:56:29 +0300

    Add another lessons

--- tests/test_lesson1.py
+++ tests/test_lesson1.py
@@ -1,5 +1,5 @@
 from lesson_1 import repetition
-
+from lesson_1 import practice
 
 def test_string_len():
     assert repetition.string_length == 23
@@ -29,3 +29,25 @@ def numbers_from_string():
 
 def lower_string():
     assert repetition.result_string == repetition.upper_string.lower()
+
+
+def test_abs():
+    assert isinstance(practice.value, int)
+    assert practice.value == 42
+
+
+def test_symbols_in_names():
+
+    assert practice.symbols_in_names == 12
+
+
+def test_interpolation():
+    assert practice.arya_hungry == "Do you want to eat ?\nYes, I'm hungry, mom"
+
+
+def test_yes():
+    assert practice.without_yes == 'I say no and another no for your offer'
+
+
+def test_upper():
+    assert practice.string_upper == practice.another_string.upper()
