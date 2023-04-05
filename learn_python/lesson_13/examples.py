86944e6a0389dd4103184ac460fc432bc7e309d2
a.cheremushkin <a.ch3re359@yandex.ru>
2023-04-05 15:07:20 +0300

    Add lesson 14

--- learn_python/lesson_13/examples.py
+++ learn_python/lesson_13/examples.py
@@ -1,18 +1,1 @@
-from collections import defaultdict
 
-_dict = {}
-
-value = 1
-_dict.setdefault('key', []).append(value)
-
-_dict.setdefault('key', []).append('hi')
-
-
-################
-
-
-def_dict = defaultdict(list)
-
-def_dict['a'].append('hello')
-
-def_dict['a'][0] = 'hi'
---
+++ learn_python/lesson_14/examples.py
@@ -0,0 +1,49 @@
+# _set = {1, 2, 'hello', 'hello'}
+#
+# print(_set)
+#
+# # Мы с тобой знаем, как создать пустой словарь. Делается это так:
+#
+# _dict = {}
+#
+# # Как мы уже сказали, литерал словаря идентичен множеству. Так как же создать пустое множество?
+#
+# empty_set = ...
+#
+# print('hello' in _set)
+# print('hello my friend' in _set)
+#
+# # Добавляем ключи в множество
+# s = set()
+# s.add('hello')
+# s.add(1)
+# s.add(2)
+# s.add(2)
+#
+# print(s)
+#
+# # Удаляем ключи из множества:
+#
+# s.discard('hello')
+# print(s)
+# s.discard('hello')
+# print(s)
+# s.remove(1)
+# print(s)
+# s.remove(1)
+
+# new_set = {'hello', 'key2', 1, 2}
+#
+# copied_set = new_set.copy()
+#
+# print(new_set, copied_set)
+# print(new_set is copied_set)
+# print(new_set == copied_set)
+#
+# copied_set.add('new_key')
+#
+# print(new_set, copied_set)
+#
+# new_set.clear()
+# print(new_set)
+
