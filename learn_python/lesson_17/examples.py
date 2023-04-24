f1fe9034f65c485e724b6208b52d895909611e82
a.cheremushkin <a.ch3re359@yandex.ru>
2023-04-24 15:07:42 +0300

    Add lesson 18

--- learn_python/lesson_17/examples.py
+++ learn_python/lesson_17/examples.py
@@ -1,21 +1,6 @@
-def f(*args, **kwargs):
-    return (args, kwargs)
+a = {
+    'a': 1 + 1,
+    'b': 2 + 2
+}
 
-
-print(f(1, 2, 3, 4, kx='a', ky='b', kz='c'))
-
-
-def f(x, *args, **kwargs):
-    return (x, args, kwargs)
-
-
-print(f(1, 2, 3, kx='a', ky='b'))
-
-
-def f(*args, ky=42, **kwargs):
-    return (args, ky, kwargs)
-
-
-print(f(1, 2, 3, 4, kz='c'))
-
-print(f(1, 2, 3, 4, ky='b', kz='c'))
+print(a['b'])
